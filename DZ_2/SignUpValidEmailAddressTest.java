25b85ea7b6e9710ccdc9f33de56e8ac7c3c3dd09
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-03-09 01:34:29 +0700

    Create SignUpValidEmailAddressTest.java

---
+++ DZ_2/SignUpValidEmailAddressTest.java
@@ -0,0 +1,49 @@
+package ru.gb;
+
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Olga Shestakova
+ * Date 08.03.2022
+ */
+
+@Test
+public class SignUpValidEmailAddressTest {
+    public void checkEmptyEmailSign() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "/Users/eolgi/Downloads/Android-NativeDemoApp-0.2.1.apk");
+
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        Thread.sleep(2000);
+
+        MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
+        loginMenuButton.click();
+        Thread.sleep(2000);
+
+        MobileElement buttonSignUpContainer = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"button-sign-up-container\"]/android.view.ViewGroup/android.widget.TextView");
+        buttonSignUpContainer.click();
+        Thread.sleep(2000);
+
+        MobileElement buttonSignUp = (MobileElement) driver.findElementByXPath("//android.view.ViewGroup[@content-desc=\"button-SIGN UP\"]/android.view.ViewGroup");
+        buttonSignUp.click();
+        Thread.sleep(2000);
+
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
+        Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
+    }
+}
