4c05cb592d0122321b3f5b4f3766ec2f1f67fe4e
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-03-25 14:10:43 +0700

    Create ScreenShotHelper.java

---
+++ DZ_3/src/test/java/ru/gb/ScreenShotHelper.java
@@ -0,0 +1,23 @@
+package ru.gb;
+
+import io.appium.java_client.MobileBy;
+import org.testng.annotations.Test;
+import ru.gb.base.BaseTest;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Created by Olga Shestakova
+ * Date 24.03.2022
+ */
+public class ScreenShotHelper extends BaseTest {
+
+    @Test
+    public void createScreen() {
+        openApp();
+        File actualScreenshot = $(MobileBy.AccessibilityId("Home-screen")).screenshot();
+    }
+}
+
