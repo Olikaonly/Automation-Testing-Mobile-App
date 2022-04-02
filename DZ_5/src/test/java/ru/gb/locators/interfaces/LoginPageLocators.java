437e86f0fd08d62513ce06979965838b55e12b07
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-04-02 17:21:46 +0700

    Create LoginPageLocators.java

---
+++ DZ_5/src/test/java/ru/gb/locators/interfaces/LoginPageLocators.java
@@ -0,0 +1,13 @@
+package ru.gb.locators.interfaces;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by Olga Shestakova
+ * Date 02.04.2022
+ */
+public interface LoginPageLocators {
+    By loginButton();
+    By loginErrorText();
+}
+
