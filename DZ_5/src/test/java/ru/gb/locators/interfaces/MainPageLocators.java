74534c89f9ce4fbab3adef5f6390af2d9b1973d1
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-04-02 17:21:48 +0700

    Create MainPageLocators.java

---
+++ DZ_5/src/test/java/ru/gb/locators/interfaces/MainPageLocators.java
@@ -0,0 +1,14 @@
+package ru.gb.locators.interfaces;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by Olga Shestakova
+ * Date 02.04.2022
+ */
+public interface MainPageLocators {
+
+    By loginButton();
+    By homeScreen();
+}
+
